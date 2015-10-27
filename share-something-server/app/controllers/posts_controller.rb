9e863bc26435129bcf19328ac5f47c054fece5ca
Julian Bouma <julian@devbootcamp.com>
2015-10-26 18:52:13 -0700

    fill out posts controller

--- share-something-server/app/controllers/posts_controller.rb
+++ share-something-server/app/controllers/posts_controller.rb
@@ -1,17 +1,33 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+    render json: @posts
   end
 
   def show
+    render json: @post
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      render json: @post, status: :created, location: @post
+    else
+      err
+    end
   end
 
   def update
+    if @post.update(post_params)
+      head :no_content
+    else
+      err
+    end
   end
 
   def delete
+    @post.destroy
+    head :no_content
   end
 
   private
@@ -22,5 +38,9 @@ class PostsController < ApplicationController
 
   def post_params
     params.permit(:title, :author, :date, :body)
+  end
+
+  def err
+    render json: @post.errors, status: :unprocessable_entity
   end
 end
